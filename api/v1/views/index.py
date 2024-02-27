ce2803edb49a08eb521a4ebf33784247b4de5bd3
wahujos <jwahu30@gmail.com>
2024-02-27 10:36:41 +0300

    index doc

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-'''
-index.py file of our flask application
+'''index.py file of our flask application
 adding more documentation to the index file
 '''
 
-# from api.v1.views import app_views
 from . import app_views
 from flask import jsonify
 from models import storage
@@ -12,8 +10,7 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    '''
-    Checks the status and returns a json
+    '''Checks the status and returns a json
     This function is run and returns a json file showing status okay
     this is done in json
     '''
@@ -25,8 +22,7 @@ def status():
 
 @app_views.route('/stats', methods=['GET'])
 def retrieve_number():
-    '''
-    Create an endpoint that retrieves the number of each objects by type
+    '''Create an endpoint that retrieves the number of each objects by type
     This shows the stats we have in the database
     '''
     response = {}
