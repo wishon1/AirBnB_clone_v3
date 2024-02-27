0e3e704906b95c085b95487dc9d0791fe1063f26
wahujos <jwahu30@gmail.com>
2024-02-27 10:18:18 +0300

    added stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 # from api.v1.views import app_views
 from . import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -13,3 +14,22 @@ def status():
             "status": "OK"
             }
     return jsonify(data)
+
+
+@app_views.route('/stats', methods=['GET'])
+def retrieve_number():
+    """
+    Create an endpoint that retrieves the number of each objects by type:
+    """
+    response = {}
+    objects = {
+            "Amenity": "iamenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+            }
+    for key, value in objects.items():
+        response[value] = storage.count(key)
+    return jsonify(response)
